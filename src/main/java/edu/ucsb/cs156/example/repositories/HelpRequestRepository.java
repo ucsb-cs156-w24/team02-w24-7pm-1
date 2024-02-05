1ff3cf342263a84214a77ba252aa75b8a56a361d
Daoyijiucsb <daoyi@umail.ucsb.edu>
2024-02-04 16:53:35 -0800

    dj - add datebase

--- src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
@@ -3,11 +3,11 @@ package edu.ucsb.cs156.example.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import main.java.edu.ucsb.cs156.example.entities.HelpRequest;
+import edu.ucsb.cs156.example.entities.HelpRequest;
 
 
 
 @Repository
 public interface HelpRequestRepository extends CrudRepository<HelpRequest, Long> {
-
+    Iterable<HelpRequest> findAllByRequestTime(String requestTime);
 }
