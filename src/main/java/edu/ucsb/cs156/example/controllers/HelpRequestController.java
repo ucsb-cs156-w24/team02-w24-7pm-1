d737ec06d1b52b26199c305307dc71b480bde1cd
Daoyijiucsb <daoyi@umail.ucsb.edu>
2024-02-05 19:57:42 -0800

    dj - add datebase
    dj - add datebase
    
    dj - add hr datebase controller
    
    dj - add hr datebase Controller GET

--- src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestController.java
@@ -2,16 +2,11 @@ package edu.ucsb.cs156.example.controllers;
 
 import java.time.LocalDateTime;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -73,7 +68,7 @@ public class HelpRequestController extends ApiController {
 
         return savedHelpRequest;
     }
-/* 
+
     @Operation(summary= "Get a single help request")
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("")
@@ -83,7 +78,7 @@ public class HelpRequestController extends ApiController {
                 .orElseThrow(() -> new EntityNotFoundException(HelpRequest.class, id));
 
         return helpRequest;
-    }
+    }/*
 
     @Operation(summary= "Delete a help request")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
