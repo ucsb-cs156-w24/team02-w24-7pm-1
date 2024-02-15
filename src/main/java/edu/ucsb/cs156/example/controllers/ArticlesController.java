ad95e8012ad27bc3a0c3e349b66af5f0db8c59d3
liamlizard <82777456+liamlizard@users.noreply.github.com>
2024-02-14 19:47:48 -0800

    Fixed hopefully

--- src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
@@ -86,36 +86,5 @@ public class ArticlesController extends ApiController{
         return articles;
     }
 
-    @Operation(summary= "Update a single article")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @PutMapping("")
-    public Articles updateArticle(
-            @Parameter(name="id") @RequestParam Long id,
-            @RequestBody @Valid Articles incoming) {
 
-        Articles article = articlesRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
-
-        article.setTitle(incoming.getTitle());
-        article.setUrl(incoming.getUrl());
-        article.setExplanation(incoming.getExplanation());
-        article.setEmail(incoming.getEmail());
-        article.setDateAdded(incoming.getDateAdded());
-
-        articlesRepository.save(article);
-
-        return article;
-    }
-    
-    @Operation(summary= "Delete an Article")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @DeleteMapping("")
-    public Object deleteArticle(
-            @Parameter(name="id") @RequestParam Long id) {
-        Articles article = articlesRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
-
-        articlesRepository.delete(article);
-        return genericMessage("Articles with id %s deleted".formatted(id));
-    }
 }
